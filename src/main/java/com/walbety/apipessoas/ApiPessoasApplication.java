0b2e3283e0f58fc25a7d93e0f19f65bbf909bd5e
Walbety Castro <walbety.castro.araujo@everis.com>
2020-03-24 12:52:30 -0300

    primeiro commit; projeto criado

---
+++ src/main/java/com/walbety/apipessoas/ApiPessoasApplication.java
@@ -0,0 +1,13 @@
+package com.walbety.apipessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walbety/apipessoas/ApiPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walbety.apipessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
